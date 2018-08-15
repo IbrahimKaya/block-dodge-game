f58452414d171ccb9521e9e17cca4915ffa552ac
IbrahimKaya <kayaibrahim0@yandex.com>
2018-08-15 19:52:14 +0300

    core game

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public float slowTime = 10f;
+
+	public void EndGame()
+    {
+        StartCoroutine(RestartLevel());
+    }
+
+    IEnumerator RestartLevel()
+    {
+        Time.timeScale =1f/ slowTime;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowTime;
+
+        yield return new WaitForSeconds(1f / slowTime);
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowTime;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 15f;
+    public float mapWidth = 6;
+
+    private Rigidbody2D rb;
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+    public void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal")*Time.fixedDeltaTime*speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        rb.MovePosition(newPosition);
+    }
+
+     void OnCollisionEnter2D()
+    {
+        FindObjectOfType<GameManager>().EndGame();
+    }
+}
